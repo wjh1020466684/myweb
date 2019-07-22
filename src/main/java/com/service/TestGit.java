10184c1fa6e95091da8c9caf2e526b9739b13831
wjh1020466684 <53166662+wjh1020466684@users.noreply.github.com>
2019-07-22 15:20:00 +0800

    Add files via upload

---
+++ src/main/java/com/service/TestGit.java
@@ -0,0 +1,10 @@
+package com.service;
+
+public class TestGit {
+
+    public  static  void main(String[] args){
+        System.out.println("王嘉豪");
+        System.out.println("hello");
+        System.out.println("hello123");
+    }
+}
